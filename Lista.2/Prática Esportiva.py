2212f32cda0e81dbcd9a1b80280bd9a69c911c5d
Carolmstn <carolmestriner@outlook.com.br>
2026-08-31 11:23:55 -0300

    Create Prática Esportiva.py

---
+++ Lista.2/Prática Esportiva.py
@@ -0,0 +1,8 @@
+print ("Prática Esportiva\n")
+
+a = int(input("Digite sua idade: "))
+b = input("Você possui autorização? ")
+
+if (a >= 12 and a <= 18) and b == "sim":
+    print("Entrada autorizada")
+
